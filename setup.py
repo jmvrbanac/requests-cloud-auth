882790011c0cf117f05205cbcbe5d48497711136
John Vrbanac <john.vrbanac@linux.com>
2014-09-29 19:05:47 -0500

    Adding base structure

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='requests-cloud',
+    version='0.0.1',
+    description=(''),
+    long_description=(''),
+    url='',
+    author='John Vrbanac',
+    author_email='john.vrbanac@linux.com',
+    license='Apache v2',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='',
+    packages=find_packages(exclude=['contrib', 'docs', 'spec*']),
+    install_requires=['requests>=2.4.1'],
+    package_data={},
+    data_files=[],
+    entry_points={
+        'console_scripts': [],
+    },
+)
