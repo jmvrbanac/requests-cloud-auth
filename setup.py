d3e231e0c4eff309b90521f8deadd2ac9c93f454
John Vrbanac <john.vrbanac@linux.com>
2015-02-15 01:38:57 -0600

    Adding Python 3.3 and 3.4

--- setup.py
+++ setup.py
@@ -18,6 +18,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
 
     keywords='',
