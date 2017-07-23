068dd1e1fee9a6345e18d0a87cf5edf341f45753
John Vrbanac <john.vrbanac@linux.com>
2017-07-23 11:10:05 -0500

    Updating setup.py classifiers

--- setup.py
+++ setup.py
@@ -18,8 +18,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     keywords='requests authenication cloud keystone extensions',
